2101a08861fbc58056a7ee79387d74782ffbe135
Jackson <jackson1712@hotmail.com>
2023-04-26 12:01:55 -0300

    fix: to fix items

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,22 @@
-import { knex as setupKnex } from 'knex'
+import 'dotenv/config'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL not found')
+}
+
+console.log(process.env)
+
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL,
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './database/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
