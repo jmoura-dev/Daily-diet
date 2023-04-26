822dea82fbd8978e00faae6da1097d22a30a0382
Jackson <jackson1712@hotmail.com>
2023-04-26 18:25:50 -0300

    added migration of meals

---
+++ database/migrations/20230426181414_createMeal.ts
@@ -0,0 +1,21 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.text('isDiet').defaultTo('false')
+    table
+      .uuid('user_id')
+      .references('id')
+      .inTable('users')
+      .index()
+      .onDelete('CASCADE')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
