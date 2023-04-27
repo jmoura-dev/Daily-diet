29d2c7a7de49eafe4c02fb6c4155de6037be57bd
Jackson <jackson1712@hotmail.com>
2023-04-27 13:59:26 -0300

    added new routes in application

--- src/routes/diet.ts
+++ src/routes/diet.ts
@@ -8,7 +8,7 @@ export async function mealRoutes(app: FastifyInstance) {
     const createSchemaMeal = z.object({
       name: z.string(),
       description: z.string(),
-      isDiet: z.boolean(),
+      isDiet: z.string(),
     })
 
     const createSchemaParamsMeal = z.object({
@@ -17,13 +17,18 @@ export async function mealRoutes(app: FastifyInstance) {
 
     const { name, description, isDiet } = createSchemaMeal.parse(req.body)
     const { userId } = createSchemaParamsMeal.parse(req.params)
-    console.log(name, description, isDiet)
 
-    if (!name || !description || !isDiet) {
+    if (!name || !description) {
       throw new Error('[401] Preecha todos os campos para registar a refeição')
     }
 
     if (!userId) {
+      throw new Error('Usuário não identificado.')
+    }
+
+    const user = await knex('users').where('id', userId).first()
+
+    if (!user) {
       throw new Error('Usuário não identificado.')
     }
 
@@ -36,5 +41,97 @@ export async function mealRoutes(app: FastifyInstance) {
     })
 
     return res.status(201).send()
+  })
+
+  app.get('/:userId', async (req, res) => {
+    const createSchemaParamsMeal = z.object({
+      userId: z.string(),
+    })
+
+    const { userId } = createSchemaParamsMeal.parse(req.params)
+
+    if (!userId) {
+      throw new Error('Usuário não identificado.')
+    }
+
+    const user = await knex('users').where('id', userId).first()
+
+    if (!user) {
+      throw new Error('Usuário não identificado.')
+    }
+
+    const listMealsUser = await knex('meals').where({ user_id: userId })
+
+    if (listMealsUser.length <= 0) {
+      throw new Error('Este usuário não tem nenhuma refeição')
+    }
+
+    return res.status(201).send(listMealsUser)
+  })
+
+  app.put('/:id', async (req, res) => {
+    const createSchemaBodyMeal = z.object({
+      name: z.coerce.string(),
+      description: z.coerce.string(),
+      isDiet: z.coerce.string(),
+    })
+
+    const createSchemaParamsMeal = z.object({
+      id: z.string(),
+    })
+
+    const { id } = createSchemaParamsMeal.parse(req.params)
+    let { name, description, isDiet } = createSchemaBodyMeal.parse(req.body)
+
+    const meal = await knex('meals').where({ id }).first()
+
+    if (!id) {
+      throw new Error('Informe o endereço do prato.')
+    }
+
+    if (!meal) {
+      throw new Error('Refeição não encontrada.')
+    }
+
+    if (name === 'undefined') {
+      name = meal.name
+    }
+
+    if (description === 'undefined') {
+      description = meal.description
+    }
+
+    if (isDiet === 'undefined') {
+      isDiet = meal.isDiet
+    }
+
+    await knex('meals').where({ id: meal.id }).update({
+      name,
+      description,
+      isDiet,
+    })
+
+    return res.status(200).send()
+  })
+
+  app.delete('/:id', async (req, res) => {
+    const createSchemaParamsMeal = z.object({
+      id: z.string(),
+    })
+
+    const { id } = createSchemaParamsMeal.parse(req.params)
+    const meal = await knex('meals').where({ id }).first()
+
+    if (!id) {
+      throw new Error('Informe o endereço do prato a ser deletado.')
+    }
+
+    if (!meal) {
+      throw new Error('Refeição não encontrada.')
+    }
+
+    await knex('meals').where({ id }).delete()
+
+    return res.status(200).send()
   })
 }
